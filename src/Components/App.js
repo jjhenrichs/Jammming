5d405eba877e2ab3b2a1aa7311a769075240cf5c
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-09-05 14:45:51 -0400

    Added functionality to the Clear and Search Buttons

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,36 @@
+import React, { useState, useEffect } from "react";
 import styles from "../style/App.module.css";
 import SearchBar from "./SearchBar";
 import SearchResult from "./SearchResults";
 import Playlist from "./Playlist";
 
 function App() {
+  const [researchInput, setResearchInput] = useState("");
+
+  function handleSearchInput({ target }) {
+    console.log(target.value);
+    setResearchInput(target.value);
+  }
+
+  function handleSubmit() {
+    alert(researchInput);
+  }
+
+  function handleClearInput() {
+    setResearchInput("");
+  }
+
   return (
     <div className={styles.container}>
       <h1>
         Ja<span className={styles.highlight}>mmm</span>ing
       </h1>
-      {/* <p>`${process.env.REACT_APP_CLIENT_ID}`</p> */}
-      <SearchBar />
+      <SearchBar
+        value={researchInput}
+        onChange={handleSearchInput}
+        submitQuery={handleSubmit}
+        clearQuery={handleClearInput}
+      />
       <div className={styles.dataContainer}>
         <SearchResult />
         <Playlist />
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styles from "../style/SearchBar.module.css";
 
-export default function SearchBar() {
+export default function SearchBar(props) {
   return (
     <div className={styles.container}>
       <input
         type="text"
         className={styles.input}
         placeholder="Enter a song, artist, or album"
+        value={props.value}
+        onChange={props.onChange}
       />
       <div className={styles.btnContainer}>
-        <button type="reset" className={styles.btn}>
+        <button type="reset" className={styles.btn} onClick={props.clearQuery}>
           Clear
         </button>
-        <button type="submit" className={styles.btn}>
+        <button
+          type="submit"
+          className={styles.btn}
+          onClick={props.submitQuery}
+        >
           Search
         </button>
       </div>
