ec3a4644ce8e593fd97fd6975c5a0f7b1b57c57f
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-11-07 15:02:36 -0500

    Hardcording clientId

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -2,7 +2,7 @@ const clientId = "334852632e8f48958ecfcaddff502b60";
 const redirect_uri = "https://jammming-jjhenrichs.netlify.app";
 
 let url = "https://accounts.spotify.com/authorize";
-url += `?client_id=${clientId}`;
+url += `?client_id=334852632e8f48958ecfcaddff502b60`;
 url += "&response_type=token";
 url += "&scope=playlist-modify-public";
 url += `&redirect_uri=${redirect_uri}`;
