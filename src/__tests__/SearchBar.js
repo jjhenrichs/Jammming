1a992c2519b4c0c73d3c8464cd850d613a7c4b59
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-11-09 22:47:46 -0500

    Finished SearchBar testing suite

--- src/__tests__/SearchBar.js
+++ src/__tests__/SearchBar.js
@@ -21,4 +21,35 @@ describe("SearchBar", () => {
     userEvent.type(inputField, "Tiny Dancer");
     expect(inputField).toHaveValue("Tiny Dancer");
   });
+
+  it("displays Seacrch Button", () => {
+    render(<SearchBar />);
+    const searchButton = screen.getByRole("button", { name: /Search/i });
+    expect(searchButton).toBeInTheDocument();
+  });
+
+  it("displays Clear button", () => {
+    render(<SearchBar />);
+    const clearButton = screen.getByRole("button", { name: /Clear/i });
+    expect(clearButton);
+  });
+
+  it("clears the search bar when clear is clicked", () => {
+    render(<SearchBar />);
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "Jump");
+    const clearButton = screen.getByRole("button", { name: /Clear/i });
+    userEvent.click(clearButton);
+    expect(input).toBeEmptyDOMElement();
+  });
+
+  it("sumbit a song query when Search is pressed", () => {
+    const searchFunction = jest.fn();
+    render(<SearchBar submitQuery={searchFunction} />);
+    const input = screen.getByRole("textbox");
+    const search = screen.getByRole("button", { name: /Search/i });
+    userEvent.type(input, "Jump");
+    userEvent.click(search);
+    expect(searchFunction).toBeCalled();
+  });
 });
